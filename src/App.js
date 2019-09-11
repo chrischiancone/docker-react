b8a3da99a6fc51b07d5eab644cf0778d237c9a12
Chris Chiancone <cchiancone@outlook.com>
2019-09-11 09:17:59 -0500

    change app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Formerly Encryptics
+          Formerly Encryptics!
         </a>
       </header>
     </div>
